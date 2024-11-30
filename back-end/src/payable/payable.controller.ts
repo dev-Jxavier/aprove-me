f394f43f9b7821cd3663022245803c9e8be3e9ae
dev-Jxavier <joaovitor210102@gmail.com>
2024-11-30 09:13:10 -0300

    feat: :sparkles: add payable CRUD

--- back-end/src/payable/payable.controller.ts
+++ back-end/src/payable/payable.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreatePayableDto } from './dto/create-payable.dto';
 import { PayableService } from './payable.service';
+import { UpdatePayableDto } from './dto/update-payable.dto';
 
-@Controller('integrations')
+@Controller('integrations/payable')
 export class PayableController {
     constructor(private readonly payableService: PayableService) { }
 
-    @Post('/payable')
+    @Post('/')
     create(@Body() createPayableDto: CreatePayableDto) {
         return this.payableService.create(createPayableDto);
+    }
+
+    @Get(':id')
+    getById(@Param('id') id: string) {
+        return this.payableService.getById(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updatePayableDto: UpdatePayableDto) {
+        return this.payableService.update(id, updatePayableDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.payableService.remove(id);
     }
 
 }
--- back-end/src/payable/payable.service.ts
+++ back-end/src/payable/payable.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreatePayableDto } from './dto/create-payable.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Payable } from '@prisma/client';
+import { UpdatePayableDto } from './dto/update-payable.dto';
 
 @Injectable()
 export class PayableService {
@@ -9,5 +10,17 @@ export class PayableService {
 
     async create(data: CreatePayableDto): Promise<Payable> {
         return this.prismaService.payable.create({ data })
+    }
+
+    async getById(id: string) {
+        return this.prismaService.payable.findUnique({ where: { id } })
+    }
+
+    async update(id: string, updatePayableDto: UpdatePayableDto) {
+        return this.prismaService.payable.update({ where: { id }, data: updatePayableDto })
+    }
+
+    async remove(id: string) {
+        await this.prismaService.payable.delete({ where: { id } })
     }
 }
