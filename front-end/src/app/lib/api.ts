bca340610bde4366085315db09b3d31b8e99cb2c
dev-Jxavier <joaovitor210102@gmail.com>
2024-12-02 17:45:02 -0300

    feat: :sparkles: add authenticate in api

--- front-end/src/app/lib/api.ts
+++ front-end/src/app/lib/api.ts
@@ -1,7 +1,34 @@
 import axios from "axios";
+import { deleteLocalStorage, getLocalStorage } from "./localStorage";
+import { TOKEN_BAKNME } from "./constants-local-storage";
 
 const instance = axios.create({
     baseURL: 'http://localhost:3000',
 });
+
+instance.interceptors.response.use(function (response) {
+    // Qualquer código de status que dentro do limite de 2xx faz com que está função seja acionada
+    // Faz alguma coisa com os dados de resposta
+    return response;
+}, function (error) {
+    if (error.status === 403) { //Caso user não esteja logado
+        deleteLocalStorage(TOKEN_BAKNME)
+        window.location.href = '/'
+    }
+    return Promise.reject(error);
+});
+
+instance.interceptors.request.use(function (config) {
+    const token = getLocalStorage(TOKEN_BAKNME)
+
+    config.headers['Authorization'] = token
+
+    return config;
+}, function (error) {
+    // Faz alguma coisa com o erro da requisição
+    return Promise.reject(error);
+});
+
+
 
 export default instance
