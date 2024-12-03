fbec46d735d2c6ea6d3baeb7b0c95a012ef9fc60
dev-Jxavier <joaovitor210102@gmail.com>
2024-12-03 09:57:13 -0300

    feat: :sparkles: add content in home page

--- front-end/src/app/dashboard/page.tsx
+++ front-end/src/app/dashboard/page.tsx
@@ -1,10 +1,17 @@
-import Header from "../Components/Header"
+import Image from "next/image"
+import Link from "next/link"
 
 const Dashboard = () => {
     return (
-        <>
-        </>
-        // <Header />
+        <div className="flex flex-col justify-center items-center mt-[300px]">
+            <Image src='/logo-bankme-large.png' alt="logo-bankme" width={250} height={250} />
+            <h1 className="text-2xl font-normal tracking-wider mt-10">Bem-vindo ao Dashboard &#128522;</h1>
+            <div className="space-x-4 mt-10">
+                <Link href={`/dashboard/payable/create`} className="bg-indigo-600 px-3 py-1.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 rounded-md">Criar pagável</Link>
+                <Link href={`/dashboard/assignor/create`} className="bg-indigo-600 px-3 py-1.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 rounded-md">Criar cedente</Link>
+                <Link href={`/dashboard/payable/list`} className="bg-indigo-600 px-3 py-1.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 rounded-md">Lista de pagáveis</Link>
+            </div>
+        </div>
     )
 }
 
